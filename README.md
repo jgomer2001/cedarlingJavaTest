509f705f7964c9d376d82e0e71ea2e10e7da8b2a
jgomer2001 <bonustrack310@gmail.com>
2026-05-04 11:01:32 -0500

    feat: add benchmark project
    Signed-off-by: jgomer2001 <bonustrack310@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
-# cedarlingJavaTest
-Benchmarking Cedarling Java performance
+# Benchmarking Cedarling Java performance
+
+## Requirements
+
+- Java 17 or higher
+- Maven 3.8 or higher
+- Jans Server
+
+## What this test does
+
+Here we measure how long it takes for Java Cedarling to evaluate token-based authorization authorization on a simple resource against a policy.
+
+The resource (`student`) looks like: `{ "name": "John Doe", "grad_year": 2023 }`
+
+The policy:
+
+```
+@id("alumni_restricted_access")
+permit(
+  principal,
+  action in Jans::Action::"Search",
+  resource is Jans::student
+)
+when {
+  resource.grad_year < 2026 ||
+  (
+    context has tokens.jans_userinfo_token &&
+    context.tokens.jans_userinfo_token.hasTag("role") &&
+    context.tokens.jans_userinfo_token.getTag("role").contains("AdmissionsCounselor")
+  )
+};
+```
+
+No extra context data supplied.
+
+## How it works
+
+Once a (`UserInfo`) token is obtained, the class `co.MyCedarling` generates a number of random student resources, with 
+`grad_year` between 2024 and 2027. The time taken by each execution of method `authorizeMultiIssuer` is measured. The (native) time taken by Cedarling alone is extracted from the decision logs as well.
+
+Average times in microseconds are printed to stdout.
+
+## How to run
+
+```
+export JAVA_HOME= ...
+mvn compile
+mvn exec:java -Dexec.mainClass=co.MyCedarling -Dexec.args="1000 config.json eyJraWQiOiJjb25uZWN0...."
+```
+
+Execution parameters in order:
+
+- Number of random students (1000 in the above example)
+- Path to configuration file
+- UserInfo token
+
+The contents of the config file used were:
+
+```
+{
+    "CEDARLING_APPLICATION_NAME": "none",
+    "CEDARLING_JWT_SIG_VALIDATION": "enabled",
+    "CEDARLING_JWT_STATUS_VALIDATION": "enabled",
+    "CEDARLING_JWT_SIGNATURE_ALGORITHMS_SUPPORTED": [
+        "HS256",
+        "RS256"
+    ],
+    "CEDARLING_LOG_TYPE": "memory",
+    "CEDARLING_LOG_LEVEL": "INFO",
+    "CEDARLING_LOG_TTL": 60,
+    "CEDARLING_POLICY_STORE_URI": "https://github.com/jgomer2001/CedarlingQuickstart/releases/download/v0.0.4/tarpDemo.cjar"
+}
+```
+
+## Results
+
+On a Basic [DO](https://slugs.do-api.dev/) VM (s-4vcpu-8gb / Ubuntu 22) with Amazon Corretto-17.0.19 and Janssen Server 2.0.0 (AS and config-api only) alongside, the following was obtained:
+
+- Average decision time: ~2.7ms
+- Average decision time (Cedarling only): ~1.8ms
+- 1000 random students
---
+++ src/main/java/co/MyCedarling.java
@@ -0,0 +1,106 @@
+package co;
+
+import io.jans.cedarling.binding.wrapper.CedarlingAdapter;
+import io.jans.util.Pair;
+
+import java.nio.file.*;
+import java.util.*;
+import java.security.*;
+
+import org.json.*;
+import uniffi.cedarling_uniffi.*;
+
+//mvn -o exec:java -Dexec.mainClass=co.MyCedarling -Dexec.args="1000 config.json ey....blah..."
+//
+public class MyCedarling {
+    
+    private CedarlingAdapter cedarlingAdapter = new CedarlingAdapter();   
+    private Random ranma = new SecureRandom();
+
+    public static void main(String ...args) throws Exception {
+        new MyCedarling().run(Integer.parseInt(args[0]), args[1], args[2]);        
+    }
+    
+    private void run(int n, String propertiesFile, String userInfoToken) throws Exception {
+        
+        String config = Files.readString(Paths.get(propertiesFile));
+        cedarlingAdapter.loadFromJson(config);
+        log("Cedarling initialized with\n" + config);
+        
+        Map<String, String> tokens = Map.of("Jans::Userinfo_token", userInfoToken);
+        int ignored = 0;
+        
+        long decisionJava = 0, decisionNative = 0;
+        for (int i = 0; i < n; i++) {
+            //Generate random student with grad_year in [2024, 2027]
+            Map<String, Object> student = rndStudent("" + i, 2024, 2028);
+            
+            Pair<Long, Long> times = authorize(tokens, "Jans::Action::\"Search\"", student, new JSONObject());
+            Long tNative = times.getSecond();
+            
+            if (tNative != null) {
+                decisionNative += tNative;
+                decisionJava += times.getFirst();
+            } else {
+                //ignore this authorization if native time was missing in cedarling log
+                ignored++;
+            }            
+        }
+        int total = n - ignored;
+        log("Native average decision time (μs): " + String.format("%.3f", 0.001f * decisionNative / total));
+        log("Java average decision time (μs): " + String.format("%.3f", 0.001f * decisionJava / total));
+        log("Total evaluations: " + total);
+        
+    }
+    
+    private Pair<Long, Long> authorize(Map<String, String> tokens, String action, Map<String, Object> resource,
+            JSONObject context) throws Exception {
+
+        List<TokenInput> tokenInputs = new ArrayList<>();
+        tokens.entrySet().forEach(e -> tokenInputs.add(new TokenInput(e.getKey(), e.getValue())));        
+            
+        long elapsed = System.nanoTime();
+        MultiIssuerAuthorizeResult res = cedarlingAdapter.authorizeMultiIssuer(tokenInputs, action,
+                new JSONObject(resource), context);
+        elapsed = System.nanoTime() - elapsed;     
+
+        List<String> decisionLogs = cedarlingAdapter.getLogsByRequestId(res.getRequestId());
+        Long nativeTime = null;
+        
+        if (!decisionLogs.isEmpty()) {
+            JSONObject job = new JSONObject(decisionLogs.get(0));
+            nativeTime = job.optLongObject("decision_time_micro_sec", null);
+        }
+        return new Pair<>(elapsed, 1000*nativeTime);    //nano seconds
+
+    }
+    
+    private Map<String, Object> rndStudent(String id, int min, int max) {
+        
+        Map<String, Object> student = new HashMap<>(
+                Map.of("grad_year", getADecimal(min, max))
+        );
+        student.putAll(
+            Map.of("cedar_entity_mapping",
+                Map.of(
+                    "entity_type", "Jans::student",
+                    "id", id
+                )
+            )
+        );
+        student.put("name", "Joe " + id);
+        return student;
+        
+    }
+    
+    private int getADecimal(int min, int max) {
+        //Pick a uniformly distributed random number from the range [min, max)
+        return ranma.nextInt(max - min) + min;
+    }
+    
+    private static void log(String msg) {
+        System.out.println(msg);
+    }
+
+    
+}
